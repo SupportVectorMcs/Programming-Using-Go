65aa59c8a0e3ce70ed96de98ab24bcd58afb9f1f
RonHe <RonHe@RonHe.local>
2014-12-01 21:54:55 -0500

    formatting

--- functions/functions.go
+++ functions/functions.go
@@ -9,7 +9,7 @@ import (
 )
 
 /*==============================================================================
- * 1. Gauss' formula. (Lecture 2)
+ * 1. Gauss' formula.
  *
  * Implement a function that takes a positive integer n, and returns
  * the sum of the integers from 1 to n.
@@ -25,7 +25,7 @@ func SumOfFirstNIntegers(n int) int {
 }
 
 /*==============================================================================
- * 2. Speed of a Marathoner (lecture 2)
+ * 2. Speed of a Marathoner
  *
  * Write a function TimeToRun(marathonHours, marathonMinutes, miles) that
  * takes: the time a runner ran a marathon in possibly fractional hours and
@@ -51,7 +51,7 @@ func TimeToRun(marathonHours, marathonMinutes, miles float64) float64 {
 }
 
 /*==============================================================================
- * 3. Generalized Fibonacci sequences (Lecture 3)
+ * 3. Generalized Fibonacci sequences
  *
  * Implement a function GenFib(a0,a1,n) that takes: two positive integers a0,
  * a1 and a positive integer n, and returns the nth item in the sequence
@@ -70,7 +70,7 @@ func GenFib(a0, a1, n int) int {
 }
 
 /*==============================================================================
- * 4. Reversing Integers (Lecture 3)
+ * 4. Reversing Integers
  *
  * Write a function ReverseInteger(n) that takes an integer, and returns 
  * the integer  formed by reversing the decimal digits of n. For example:
@@ -93,7 +93,7 @@ func ReverseInteger(n int) int {
 }
 
 /*==============================================================================
- * 5. Growth of a Population (Lecture 3)
+ * 5. Growth of a Population
  *
  * The size at time t of a population with a birth rate r can be modeled as:
  *
@@ -122,7 +122,7 @@ func PopSize(r, x_0 float64, max_t int) float64 {
 }
 
 /*==============================================================================
- * 6. Hailstone function (Lecture 3)
+ * 6. Hailstone function
  *
  * The Hailstone function h(n) is defined as n/2 if n is even or 3n+1 if n is
  * odd.  The Hailstone sequence for n is defined by repeatedly applying this
@@ -131,8 +131,8 @@ func PopSize(r, x_0 float64, max_t int) float64 {
  *      h(n),  h(h(n)),  h(h(h(n))), ...
  *
  * It's conjectured that for all n, this sequence eventually returns to 1.
- * Write a function HailstoneReturnsTo1(n) to compute the smallest number of times h
- * must be applied to n before the sequence returns to 1.
+ * Write a function HailstoneReturnsTo1(n) to compute the smallest number of 
+ * times h must be applied to n before the sequence returns to 1.
  *============================================================================*/
 
 func h(n int) int {
@@ -154,7 +154,7 @@ func HailstoneReturnsTo1(n int) int {
 }
 
 /*==============================================================================
- * 7. Hailstone function maximum (Lecture 3)
+ * 7. Hailstone function maximum
  *
  * Write a function MaxHailstoneValue(n) that takes an integer, and returns the
  * maximum value that the Hailstone sequence:
@@ -177,7 +177,7 @@ func MaxHailstoneValue(n int) int {
 }
 
 /*==============================================================================
- * 8. Find the kth digit of an integer n (Lecture 4)
+ * 8. Find the kth digit of an integer n
  *
  * Implement a function that takes an integer n, and a positive integer k and
  * returns the k-th decimal digit of n, with digit 1 being the rightmost (least
@@ -192,7 +192,7 @@ func KthDigit(n int, k int) int {
 }
 
 /*===========================================================================
- * 9. Hypergeometric distribution (Lecture 4)
+ * 9. Hypergeometric distribution
  *
  * Write a function Hypergeometric(M,N,n,k) that takes 4 integers and returns
  * a float64 which is the value of the hypergeometric distribution 
