21d13225a0ebf24e981ae82223a026ada1422132
SupportVectorMcs <ronhe44@gmail.com>
2014-12-09 21:54:13 +0000

    debug

--- algorithms/secondSmallest/secondSmallest.go
+++ algorithms/secondSmallest/secondSmallest.go
@@ -3,13 +3,14 @@ package main
 import "fmt"
 
 func secondSmallest(L []int) int {
+    var prev, cur int
     if L[0] < L[1] {
-	prev, cur := L[1], L[0]
+	prev, cur = L[1], L[0]
     } else {
-	prev, cur := L[0], L[1]
+	prev, cur = L[0], L[1]
     }
     
-    for i := 2; i < len[L]; i++ {
+    for i := 2; i < len(L); i++ {
 	if L[i] < cur {
 	    cur = L[i]
 	    prev = cur
